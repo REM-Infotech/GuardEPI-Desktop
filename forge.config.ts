f256643638fc20655d7dcfaa5c8f11ef4c83e5e4
Nicholas Silva <nicholasazs213@gmail.com>
2026-01-19 11:30:36 -0400

    ✨ refact: Remover componentes não utilizados     Remoção de declarações de componentes que não estão mais em uso no projeto, para manter o código limpo e organizado.
    Arquivos Alterados:
        - pnpm-workspace.yaml: Adiciona dependência
        - types/plugins/components.d.ts: Remove componentes

--- forge.config.ts
+++ forge.config.ts
@@ -1,19 +1,20 @@
 import { MakerDeb } from "@electron-forge/maker-deb";
 import { MakerRpm } from "@electron-forge/maker-rpm";
-import { MakerSquirrel } from "@electron-forge/maker-squirrel";
 import { MakerZIP } from "@electron-forge/maker-zip";
 import { FusesPlugin } from "@electron-forge/plugin-fuses";
 import { VitePlugin } from "@electron-forge/plugin-vite";
 import type { ForgeConfig } from "@electron-forge/shared-types";
 import { FuseV1Options, FuseVersion } from "@electron/fuses";
-
+import SquirrelMaker from "./makers/SquirrelMaker";
+import WixMaker from "./makers/WixMaker";
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
   },
   rebuildConfig: {},
   makers: [
-    new MakerSquirrel({}),
+    WixMaker,
+    SquirrelMaker,
     new MakerZIP({}, ["darwin"]),
     new MakerRpm({}),
     new MakerDeb({}),
